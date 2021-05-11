1fde6a6f5d2e6a382ea27d9c4816af3daafbd9b9
lennard.boediger <lennard@nitrokey.com>
2021-05-11 13:58:43 +0200

    pull fix

--- pull.sh
+++ pull.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 
 mkdir git-magic
 cd git-magic
@@ -9,4 +9,13 @@ git remote add --mirror=fetch secondary https://git.dotplex.com/lennard.boediger
 git remote set-url secondary https://oauth2:$GITLAB_REPO_KEY@git.dotplex.com/lennard.boediger/mirror-test.git
 git fetch origin
 git push secondary --tags
-git push secondary --all
+
+for branch in $(git for-each-ref --format='%(refname)' refs/heads/); do
+	if [ "master" = "${branch#*/*/}" ]; then
+	    echo "${branch#*/*/}"
+	    git push secondary ${branch#*/*/}
+	else
+	    echo "${branch#*/*/}"
+	    git push secondary ${branch#*/*/} --force
+	fi
+done
